1dba504e2ac324b9b628d22055e272a6c73ff541
styphinsontoms <styphinsontoms@gmail.com>
2016-07-06 07:35:57 +0530

    Pusing Spring Security Login Implmentation.

---
+++ src/main/java/com/example/emp/web/security/presentation/LoginManager.java
@@ -0,0 +1,30 @@
+package com.example.emp.web.security.presentation;
+
+import java.io.IOException;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+@ManagedBean
+@RequestScoped
+public class LoginManager {
+
+    public String doLogin() throws IOException, ServletException {
+
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+
+        RequestDispatcher dispatcher = ((ServletRequest) context.getRequest()).getRequestDispatcher("/j_spring_security_check");
+
+        dispatcher.forward((ServletRequest) context.getRequest(), (ServletResponse) context.getResponse());
+
+        FacesContext.getCurrentInstance().responseComplete();
+        return null;
+    }
+
+}
---
+++ src/main/java/com/example/emp/web/security/presentation/LoginMbean.java
@@ -0,0 +1,13 @@
+package com.example.emp.web.security.presentation;
+
+public class LoginMbean {
+	
+	private String userName;
+	private String passWd;
+
+	public void validateUsernamePassword()
+	{
+		
+	}
+	
+}
